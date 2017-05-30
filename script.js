8f1a670ef0cf9592c89af2c9ddecd349e84e3273
Takshak Desai <takshakd@gmail.com>
2017-05-29 23:17:17 -0400

    Fill in the blanks for bill and discharge

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-/**
- * Created with smruti.
- * User: takshakd
- * Date: 2015-02-22
- * Time: 05:52 AM
- * To change this template use Tools | Templates.
- */
-
 var smruti = angular.module('smruti', []);
 
-smruti.controller('smrutiController', function($scope) {
+smruti.controller('smrutiController', function($scope, $templateCache) {
     // $scope.templates = [
     //   {name: "Certificate", url: "certificate.html"},
     //   {name: "Bill", url: "bill.html"},
@@ -17,7 +9,8 @@ smruti.controller('smrutiController', function($scope) {
     $scope.templates = {
       certificate: {
         url: "certificate.html",
-        genFcn: genCertificate
+        genFcn: genCertificate,
+        artifcatTemplate: "cert.html"
       },
       bill: {
         url: "bill.html",
@@ -59,11 +52,15 @@ smruti.controller('smrutiController', function($scope) {
     ];
 
     $scope.billTotal = 0;
+    $scope.billRemaining = 0;
+    $scope.billAdvance = 0;
     $scope.updateBillTotal = function() {
       $scope.billTotal = 0;
-      for (var i = 0; i < $scope.billables.length; i++) {
-        $scope.billTotal += Number($scope.billables[i].amount);
-      }
+      $scope.billables.forEach(function(b) {
+        $scope.billTotal += parseInt(b.amount);
+      });
+      console.log($scope.billAdvance);
+      $scope.billRemaining = $scope.billTotal - parseInt($scope.billAdvance);
     };
 
     $scope.date = Date();
@@ -96,29 +93,42 @@ smruti.controller('smrutiController', function($scope) {
 
     $scope.generate = function() {
       $scope.template.genFcn();
-      //window.open("certificate.html", "_blank");
     };
 
     function genCertificate() {
-        var printingWindow = window.open("", "_blank");
-        //printingWindow.document.write("<p>" + $scope.date + "</p>");
-        printingWindow.document.write("<html><head><title>Certificate</title></head>");
-        printingWindow.document.write("<h3>CERTIFICATE</h3>");
-        printingWindow.document.write("<p>This is to certify that " + $scope.namePrefix + " " + $scope.firstName + " " + $scope.lastName + " " + $scope.presentPast + " under treatment at this hospital for " + $scope.treatmentReason + ".</p>");
-        printingWindow.document.write("<p>" + $scope.heSheUpper + " " + $scope.presentPast + " admitted to this hospital from " + $scope.admitDate + " and discahrged on " + $scope.dischargeDate + ".</p>");
-        printingWindow.document.write("<p>" + $scope.heSheUpper + " " + $scope.presentPast + " advised to take rest from " + $scope.restStartDate + " to " + $scope.restEndDate + ".</p>");
-        printingWindow.document.write("<p>" + $scope.heSheUpper + " " + $scope.presentPast + " advised to resume " + $scope.hisHerLower + " " + $scope.leaveFrom + " from " + $scope.resumeWorkDate + ".</p>");
-        printingWindow.document.write("<br/><br/>");
-        printingWindow.document.write("<p>Dr. K. Desai</p>");
-        printingWindow.document.write("<p>(Regn. No. G-2683)</p>");
-        printingWindow.document.write("</html>");
+      var printingWindow = window.open("", "_blank");
+      printingWindow.document.open();
+      printingWindow.document.write("<html><head><title>Certificate</title></head>");
+      printingWindow.document.write("<h3>CERTIFICATE</h3>");
+      printingWindow.document.write("<p>This is to certify that " + $scope.namePrefix + " " + $scope.firstName + " " + $scope.lastName + " " + $scope.presentPast + " under treatment at this hospital for " + $scope.treatmentReason + ".</p>");
+      printingWindow.document.write("<p>" + $scope.heSheUpper + " " + $scope.presentPast + " admitted to this hospital from " + $scope.admitDate + " and discahrged on " + $scope.dischargeDate + ".</p>");
+      printingWindow.document.write("<p>" + $scope.heSheUpper + " " + $scope.presentPast + " advised to take rest from " + $scope.restStartDate + " to " + $scope.restEndDate + ".</p>");
+      printingWindow.document.write("<p>" + $scope.heSheUpper + " " + $scope.presentPast + " advised to resume " + $scope.hisHerLower + " " + $scope.leaveFrom + " from " + $scope.resumeWorkDate + ".</p>");
+      printingWindow.document.write("<br/><br/>");
+      printingWindow.document.write("<p>Dr. K. Desai</p>");
+      printingWindow.document.write("<p>(Regn. No. G-2683)</p>");
+      printingWindow.document.write("</html>");
+      printingWindow.document.close();
     }
 
     function genBill() {
-
+      var printingWindow = window.open("", "_blank");
+      printingWindow.document.open();
+      printingWindow.document.write("<html><head><title>Bill</title></head>");
+      printingWindow.document.write("<h2>BILL</h2>");
+      printingWindow.document.write("<p></p>");
+      printingWindow.document.write("</html>");
+      printingWindow.document.close();
     }
 
-    function genDischarge() {}
+    function genDischarge() {
+      var printingWindow = window.open("", "_blank");
+      printingWindow.document.open();
+      printingWindow.document.write("<html><head><title>Discharge</title></head>");
+      printingWindow.document.write("<h2>DISCHARGE</h2>");
+      printingWindow.document.write("</html>");
+      printingWindow.document.close();
+    }
 });
 
 
