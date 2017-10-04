2f16757714145952cfde1067bcfe0795adb3f472
Takshak Desai <takshakd@gmail.com>
2017-10-04 00:22:57 -0400

    Use objects instead of primitives to avoid Angular binding issues. Clean up some CSS.

--- script.js
+++ script.js
@@ -19,42 +19,38 @@ smruti.controller('smrutiController', function($scope) {
     $scope.template = $scope.templates[name];
   };
 
-  $scope.selectedBillable = "";
+  $scope.bill = {
+    billables: [
+      {name: "Consultation", amount: 0},
+      {name: "Reconsultation", amount: 0},
+      {name: "Dressings, Inj-IM/IV", amount: 0},
+      {name: "Catheter/Blood infusion", amount: 0},
+      {name: "Plaster application", amount: 0},
+      {name: "Operation theater", amount: 0},
+      {name: "IITV usage", amount: 0},
+      {name: "Specialist equipment", amount: 0},
+      {name: "Room stay", amount: 0},
+      {name: "Nursing charges", amount: 0},
+      {name: "Surguical fees", amount: 0},
+      {name: "Ward visits", amount: 0},
+      {name: "Miscellaneous", amount: 0},
+      {name: "Radiology", amount: 0},
+      {name: "Pathology", amount: 0},
+      {name: "Implants", amount: 0},
+      {name: "Anesthesia", amount: 0},
+      {name: "Medicines and disposables", amount: 0}
+    ],
+    advance: 0,
+    total: 0,
+    balance: 0
+  };
 
-  $scope.billables = [
-    {name: "Consultation", amount: 0},
-    {name: "Reconsultation", amount: 0},
-    {name: "Dressings, Inj-IM/IV", amount: 0},
-    {name: "Catheter/Blood infusion", amount: 0},
-    {name: "Plaster application", amount: 0},
-    {name: "Operation theater", amount: 0},
-    {name: "IITV usage", amount: 0},
-    {name: "Specialist equipment", amount: 0},
-    {name: "Room stay", amount: 0},
-    {name: "Nursing charges", amount: 0},
-    {name: "Surguical fees", amount: 0},
-    {name: "Ward visits", amount: 0},
-    {name: "Miscellaneous", amount: 0},
-    {name: "Radiology", amount: 0},
-    {name: "Pathology", amount: 0},
-    {name: "Implants", amount: 0},
-    {name: "Anesthesia", amount: 0},
-    {name: "Medicines and disposables", amount: 0},
-    {name: "Advance", amount: 0}
-  ];
-
-  $scope.billBalance = 0;
-  $scope.billRemaining = 0;
-
-  $scope.updateBillBalance = function() {
-    function isAdvance(x) { return x.name === "Advance"; }
-    $scope.billBalance = 0;
-    $scope.billables.filter(x => !isAdvance(x)).forEach(function(b) {
-      $scope.billBalance += parseInt(b.amount);
+  $scope.updateBill = function() {
+    $scope.bill.total = 0;
+    $scope.bill.billables.forEach(function(b) {
+      $scope.bill.total += parseInt(b.amount);
     });
-    $scope.billables.filter(isAdvance).forEach(function(b) {
-      $scope.billBalance -= parseInt(b.amount);
-    });
+    $scope.bill.balance = $scope.bill.total - $scope.bill.advance;
   };
 
   $scope.date = Date();
@@ -92,11 +88,3 @@ smruti.controller('smrutiController', function($scope) {
     window.print();
   };
 });
-
-
-// Inputs
-var admitDate = $("input-date-admit");
-var dischargeDate = $("input-date-disc");
-
-// Outputs
-var outputElementSelector = "#section-output > h2";
